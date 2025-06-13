8958a8959684adabf67d22003e869d0c8a012e1d
Munara Tussupbekova <133093672+moonarraa@users.noreply.github.com>
2025-06-13 17:28:39 +0800

    Registration Form with Modern Design

---
+++ survey-chat-ai/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import ChatPage from './pages/ChatPage';
+import RegisterPage from './pages/RegisterPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
